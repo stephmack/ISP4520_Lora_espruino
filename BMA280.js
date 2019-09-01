fe5800a9d71a246179c0ac99f6b709b5f670ab06
stephmack <mackey.stephen@gmail.com>
2019-09-01 13:54:26 -0400

    Update BMA280.js

--- BMA280.js
+++ BMA280.js
@@ -43,7 +43,8 @@ exports = BMA280;
 exports.connectSPI = function (spi, csPin) {
   var conn = new BMA280(spi, csPin);
   var acc = conn.send([0x80|0x00,0x00]);
-  var res = new DataView(acc);
+  print (acc);
+  var res = new DataView(acc.buffer);
   print("done");
   print(res.getUint8(0,1));
   if (acc != 0xEF) conn = null;
