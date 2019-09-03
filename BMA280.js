de78e5ffe43e68e50cd5466a270f348ccbb4dbc8
stephmack <mackey.stephen@gmail.com>
2019-09-03 18:39:22 -0400

    Update BMA280.js

--- BMA280.js
+++ BMA280.js
@@ -31,11 +31,12 @@ BMA280.prototype.readAcc = function() {
   var acc_res = new ArrayBuffer(12);
   var res = new DataView(acc_res);
   res.setInt16(0,(this.send([REG.OUTX_L,0])));
-  res.setInt8(2,(this.send([REG.OUTX_H,0])));
-  res.setInt8(4,(this.send([REG.OUTY_L,0])));
-  res.setInt8(6,(this.send([REG.OUTY_H,0])));
-  res.setInt8(8,(this.send([REG.OUTY_L,0])));
-  res.setInt8(10,(this.send([REG.OUTY_H,0])));
+  res.setInt16(2,(this.send([REG.OUTX_H,0])));
+  res.setInt16(4,(this.send([REG.OUTY_L,0])));
+  res.setInt16(6,(this.send([REG.OUTY_H,0])));
+  res.setInt16(8,(this.send([REG.OUTY_L,0])));
+  res.setInt16(10,(this.send([REG.OUTY_H,0])));
+  print("A");
   var xx_L = this.send([0x82,0])[1];
   var xx_M = this.send([0x83,0])[1];
   var yy_L = this.send([0x84,0])[1];
