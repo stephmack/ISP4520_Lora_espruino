e0d5ae5383552fcb9fc62b4a412ab2dc0998aa7a
stephmack <mackey.stephen@gmail.com>
2019-09-03 09:44:33 -0400

    Update BMA280.js

--- BMA280.js
+++ BMA280.js
@@ -26,7 +26,7 @@ BMA280.prototype.init = function() {
 
 //tt
 BMA280.prototype.readAcc = function() {
-  var acc_res = new ArrayBuffer(6);
+  //var acc_res = new ArrayBuffer(6);
   //var res = new DataView(acc_res);
  // res.setInt8(0,(this.send([0x82,0])[1]));
   //res.setInt8(1,(this.send([0x83,0])[1]));
@@ -34,19 +34,22 @@ BMA280.prototype.readAcc = function() {
  // res.setInt8(3,(this.send([0x85,0])[1]));
   //res.setInt8(4,(this.send([0x86,0])[1]));
   //res.setInt8(5,(this.send([0x87,0])[1]));
-  //var xx_L = this.send([0x82,0])[1];
-  //var xx_M =this.send([0x83,0])[1];
-  //var yy_L =this.send([0x84,0])[1];
-  //var yy_M =this.send([0x85,0])[1];
-  //var zz_L =this.send([0x86,0])[1];
-  //var zz_M =this.send([0x87,0])[1];
-  //var xx = [xx_M << 6 | xx_L >> 2];
-  //var yy = [yy_M << 6 | yy_L >> 2];
-  //var zz = [zz_M << 6 | zz_L >> 2];
-  //return {
-  //  res;
-  //};
+  var xx_L = this.send([0x82,0])[1];
+  var xx_M = this.send([0x83,0])[1];
+  var yy_L = this.send([0x84,0])[1];
+  var yy_M = this.send([0x85,0])[1];
+  var zz_L = this.send([0x86,0])[1];
+  var zz_M = this.send([0x87,0])[1];
+  var xx = [xx_M << 6 | xx_L >> 2];
+  var yy = [yy_M << 6 | yy_L >> 2];
+  var zz = [zz_M << 6 | zz_L >> 2];
+  return {
+    x: xx,
+    y: yy,
+    z: zz
+  };
 };
+
 BMA280.prototype.send = function(data) {
   var res = this.spi.send(data,this.csPin);
   return res;
