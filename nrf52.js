706d60d21066c8af82b215ce54406f707a79df5b
stephmack <mackey.stephen@gmail.com>
2019-09-04 07:24:54 -0400

    Update nrf52.js

--- nrf52.js
+++ nrf52.js
@@ -4,18 +4,17 @@ var acc = require("https://github.com/stephmack/ISP4520_Lora_espruino/blob/maste
 var res = acc.readAcc();
 print(res);
 
-
-function hex_to_ascii(str1)
- {
-	//var hex  = str1.toString();
-	var str = '';
-	for (var n = 0; n < str1.length; n++) {
-		str += String.fromCharCode(str1[n]);
-	}
-	return str;
- }
-
 var t = setInterval(function () {
  var res = acc.readAcc();
  print(res);
 }, 1000);
+
+SPI1.setup({ sck:D23, miso:D25, mosi:D26 });
+
+var sx = require("https://github.com/stephmack/ISP4520_Lora_espruino/blob/master/SX127x.js").connect({spi: SPI1, cs: D24, rst : D19 });
+
+var config = {
+  rxContinuous : true
+};
+// Until DIO0 line irqs are implemented we need this:
+setInterval(function() { sx.onIRQ(); }, 100);
