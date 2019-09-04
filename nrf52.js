77e965feabebe7a5e8e5597ec4c8f2b717d53dba
stephmack <mackey.stephen@gmail.com>
2019-09-03 20:55:16 -0400

    Update nrf52.js

--- nrf52.js
+++ nrf52.js
@@ -1,22 +1,9 @@
-var spi = SPI1.setup({sck:D17, miso:D12, mosi:D10});
-var csPin =  D7; 
-var acc = require("https://https://github.com/stephmack/ISP4520_Lora_espruino/blob/master/BMA280.js").connectSPI(spi,csPin);
-acc.init();
-var ic = 1;
-var i = 0;
-var xL = 0;
-var xH = 0;
-var yL = 0;
-var yH = 0;
-var zL = 0;
-var zH = 0;
-var xP = 0;
-var yP = 0;
-var zP = 0;
-var cnt = 0;
-var tempd = 0;
+SPI1.setup({sck:D17, miso:D12, mosi:D10});
+var csPin = D7;
+var acc = require("https://github.com/stephmack/ISP4520_Lora_espruino/blob/master/BMA280.js").connectSPI(SPI1,csPin);
+var res = acc.readAcc();
+print(res);
 
-var i = 0;
 
 function hex_to_ascii(str1)
  {
@@ -29,14 +16,6 @@ function hex_to_ascii(str1)
  }
 
 var t = setInterval(function () {
-  tempd = acc.read();
-  xL = tempd.xL;
-  xH = tempd.xH;
-  yL = tempd.yL;
-  yH = tempd.yH;
-  zL = tempd.zL;
-  zH = tempd.zH;
-  xP = tempd.xP;
-  yP = tempd.yP;
-  zP = tempd.zP;
+ var res = acc.readAcc();
+ print(res);
 }, 1000);
